96419a26515740ced7c1dbc83f6a2bfdfe27d1ba
nureddyn <nureddynballout@gmail.com>
2023-10-31 19:15:03 -0300

    Task 2.* done

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+// Tasks 1.*
 const mainEl = document.querySelector('main');
 mainEl.style.backgroundColor = 'var(--main-bg)';
 mainEl.innerHTML = '<h1>SEI Rocks!</h1>';
 mainEl.classList.add('flex-ctr');
 
+// tasks 2.*
+const topMenuEl = document.querySelector("#top-menu");
+topMenuEl.style.height = '100%';
+topMenuEl.style.backgroundColor = 'var(--top-menu-bg)';
+topMenuEl.classList.add('flex-around');
