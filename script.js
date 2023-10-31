c1c897a10a1d6e673d32bfeb05a0770dfcfa7d6a
nureddyn <nureddynballout@gmail.com>
2023-10-31 19:24:27 -0300

    Task 3.* done

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+// Menu data structure
+var menuLinks = [
+    {text: 'about', href: '/about'},
+    {text: 'catalog', href: '/catalog'},
+    {text: 'orders', href: '/orders'},
+    {text: 'account', href: '/account'},
+  ];
+
 // Tasks 1.*
 const mainEl = document.querySelector('main');
 mainEl.style.backgroundColor = 'var(--main-bg)';
@@ -9,3 +17,11 @@ const topMenuEl = document.querySelector("#top-menu");
 topMenuEl.style.height = '100%';
 topMenuEl.style.backgroundColor = 'var(--top-menu-bg)';
 topMenuEl.classList.add('flex-around');
+
+// tasks 3.*
+menuLinks.forEach((link) => {
+    let anchor = document.createElement('a');
+    anchor.setAttribute('href', link.href);
+    anchor.textContent = link.text;
+    topMenuEl.append(anchor);
+});
